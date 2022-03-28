77adf9648bd7cdccdfdc097479c25e02dd91afbe
joshgoshbgosh <joshlaniermusic@gmail.com>
2022-03-27 23:30:15 -0400

    header updates

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
+import { NavLink} from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
-import Image from 'react-bootstrap/Image';
+import cleanlogo from './../../images/cleanlogo.png';
 import './index.css';
 
 function Header(props){
   return(
     <header>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-
-  <Navbar.Brand href="#home">CleanM4</Navbar.Brand>
+    <NavLink to='/'>
+     <Image className="cleanlogo" src={cleanlogo}width="87"height="87"className="d-inline-block align-top"to="/"/>
+     </NavLink>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Home</Nav.Link>
+    <Nav className="me-auto nav-menu">
+      <Nav.Link className="home-nav"href="#features">Home</Nav.Link>
       <NavDropdown title="Cleaning" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Please</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Hire</NavDropdown.Item>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { BrowserRouter as Router,Switch,Route,  Link} from "react-router-dom";
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+     <Router basename="/">
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
